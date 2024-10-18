114104d1dfd2330c1108372b404b9be390a7d0c2
mifnaufal <naufalhhdd@gmail.com>
2024-10-18 09:46:31 +0700

    post, delete, else

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,11 +6,41 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index() 
+    public function create() 
     {
 
         $posts = Post::latest()->get();
-        return view('post.index', compact('post'));
+        return view('post.index', compact(''));
+        return vew('post.create');
+    }
 
+    public function store (Request $request)
+    {
+        $this ->validate($request, [
+            'title' => 'required',
+            'content' => 'required',
+            ]);
+
+            $posts = Post::create([
+                'title' => $request->title,
+                'content' => $request->content,
+                'status' => $request->status,
+                'slug' => Str::slug($request->title)
+                ]);
+                
+            if ($posts) {
+                return redirect()
+                ->route('post.index')
+                ->with([
+                    'succes' => 'New post has been created successfully'
+                ]);
+            }else {
+                return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Some problem occoured, please try again'
+                    ]);
+            }
     }
 }
